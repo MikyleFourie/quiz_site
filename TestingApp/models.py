5a481feeea87298e2f49808a8b57d1848cb2332e
ivyycc <ivy.chepkwony@gmail.com>
2024-05-13 23:58:14 +0200

    fixing models.py in Testing App

--- TestingApp/models.py
+++ TestingApp/models.py
@@ -1,6 +1,7 @@
 from django.apps import apps
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.sessions import base_session
 from quizes.models import Quiz
 
 #tables for users
@@ -10,18 +11,18 @@ class GameSession(models.Model):
     user= models.ForeignKey(
         User,
         on_delete=models.PROTECT,
-        blank=False) #sort out this user thing.....
+        blank=False) #sort out this user thing.....ok update...it's kinda working
     
     quiz = models.ForeignKey(
         Quiz,
         on_delete=models.PROTECT,
-        blank=False  )
+        blank=False  ) #this is working
     
-    date_time = models.DateTimeField()
+    datetime = models.DateTimeField() #get datetime from django-sessions.......
 
-    score = models.IntegerField(null=True)
+    score = models.IntegerField(null=True) #must pull from score function
 
-    totalTime = models.TimeField()
+    totalTime = models.TimeField()#must pull from time function
 
     
 
