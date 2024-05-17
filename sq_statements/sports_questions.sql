905be0fd8fad23cba67b38d6a10bb517dc4f08ed
ivyycc <ivy.chepkwony@gmail.com>
2024-05-17 18:19:16 +0200

    added questions

---
+++ sq_statements/sports_questions.sql
@@ -0,0 +1,118 @@
+INSERT INTO public.quiztest_question(
+	id, "typeOfQ", title, difficulty, quiz_id)
+	VALUES (31,0,'Who won the Netball World Cup in 2023?', 2, 1);
+
+INSERT INTO public.quiztest_question(
+	id, "typeOfQ", title, difficulty, quiz_id)
+	VALUES (32,0,'Which sport uses the terms “spike”, “ace” and “libero”? ', 2, 1);
+
+INSERT INTO public.quiztest_question(
+	id, "typeOfQ", title, difficulty, quiz_id)
+	VALUES (33,0,'Which of the following leagues has Faf de Klerk played in?', 3, 1);
+
+INSERT INTO public.quiztest_question(
+	id, "typeOfQ", title, difficulty, quiz_id)
+	VALUES (34,0,'How many players are there in a cricket team? ', 1, 1);
+
+INSERT INTO public.quiztest_question(
+	id, "typeOfQ", title, difficulty, quiz_id)
+	VALUES (35,1,'As of May 2024, Javier Sotomayor holds the world record for high jump.', 2, 1);
+
+INSERT INTO public.quiztest_question(
+	id, "typeOfQ", title, difficulty, quiz_id)
+	VALUES (36,1,'There is one umpire in a game of netball. (', 1, 1);
+
+INSERT INTO public.quiztest_question(
+	id, "typeOfQ", title, difficulty, quiz_id)
+	VALUES (37,0,'What is the highest score you can receive in bowling? ', 2, 1);
+
+INSERT INTO public.quiztest_question(
+	id, "typeOfQ", title, difficulty, quiz_id)
+	VALUES (38,0,'Which of the following is not an official swimming stroke? ', 1, 1);
+
+INSERT INTO public.quiztest_question(
+	id, "typeOfQ", title, difficulty, quiz_id)
+	VALUES (39,0,'Leonardo da Vinci was vegetarian. ', 2, 1);
+
+INSERT INTO public.quiztest_question(
+	id, "typeOfQ", title, difficulty, quiz_id)
+	VALUES (40,1,'Claude Monet is most known for his paintings of what?', 2, 1);
+
+INSERT INTO public.quiztest_question(
+	id, "typeOfQ", title, difficulty, quiz_id)
+	VALUES (41,0,'Where is the Louvre art museum located?', 1, 1);
+
+INSERT INTO public.quiztest_question(
+	id, "typeOfQ", title, difficulty, quiz_id)
+	VALUES (42,0,'Vincent van Gogh died a wealthy man. ', 2, 1);
+
+INSERT INTO public.quiztest_question(
+	id, "typeOfQ", title, difficulty, quiz_id)
+	VALUES (43,0,'Which of the following instruments is part of the brass family?', 1, 1);
+
+INSERT INTO public.quiztest_question(
+	id, "typeOfQ", title, difficulty, quiz_id)
+	VALUES (44,1,'How many keys does a standard piano have?', 3, 1);
+
+INSERT INTO public.quiztest_question(
+	id, "typeOfQ", title, difficulty, quiz_id)
+	VALUES (45,1,'In the “Do-Re-Mi” song in “The Sound of Music” film, which note is referred to as “a long, long way to run”?', 2, 1);
+
+INSERT INTO public.quiztest_question(
+	id, "typeOfQ", title, difficulty, quiz_id)
+	VALUES (46,0,'Which country is the band ‘ABBA’ from? ', 2, 1);
+
+INSERT INTO public.quiztest_question(
+	id, "typeOfQ", title, difficulty, quiz_id)
+	VALUES (47,0,'Elvis Presley is known as the ‘King of Pop’', 3, 1);
+
+INSERT INTO public.quiztest_question(
+	id, "typeOfQ", title, difficulty, quiz_id)
+	VALUES (48,0,'Beyoncé has won the most Grammys ever. ', 3, 1);
+
+INSERT INTO public.quiztest_question(
+	id, "typeOfQ", title, difficulty, quiz_id)
+	VALUES (49,1,'How many members does the South Korean boy band ‘BTS’ have? ', 3, 1);
+
+INSERT INTO public.quiztest_question(
+	id, "typeOfQ", title, difficulty, quiz_id)
+	VALUES (50,0,'Which architectural style uses gargoyles as decorative elements?', 1, 1);
+
+INSERT INTO public.quiztest_question(
+	id, "typeOfQ", title, difficulty, quiz_id)
+	VALUES (51,0,'Freddie Mercury was the lead vocalist of the band ‘Queen’.',1, 1);
+
+INSERT INTO public.quiztest_question(
+	id, "typeOfQ", title, difficulty, quiz_id)
+	VALUES (52,0,'Which English City are the Beatles from?', 1, 1);
+
+INSERT INTO public.quiztest_question(
+	id, "typeOfQ", title, difficulty, quiz_id)
+	VALUES (53,1,'Stevie Nicks was a member of which rock band?', 2, 1);
+
+INSERT INTO public.quiztest_question(
+	id, "typeOfQ", title, difficulty, quiz_id)
+	VALUES (54,0,'Marina Abramović is a Polish performance artist.', 3, 1);
+
+INSERT INTO public.quiztest_question(
+	id, "typeOfQ", title, difficulty, quiz_id)
+	VALUES (55,0,'In which city will the 2024 Summer Olympics be held?', 2, 1);
+
+INSERT INTO public.quiztest_question(
+	id, "typeOfQ", title, difficulty, quiz_id)
+	VALUES (56,1,'A basketball game is 48 minutes in total.', 1, 1);
+
+INSERT INTO public.quiztest_question(
+	id, "typeOfQ", title, difficulty, quiz_id)
+	VALUES (57,1,'Baba Watson is a right hand golf player.', 3, 1);
+
+INSERT INTO public.quiztest_question(
+	id, "typeOfQ", title, difficulty, quiz_id)
+	VALUES (58,1,'Karate will be an official sport in the 2024 Olympics.', 2, 1);
+INSERT INTO public.quiztest_question(
+	id, "typeOfQ", title, difficulty, quiz_id)
+	VALUES (59,0,'What team does Erling Haaland play for? ', 1, 1);
+
+INSERT INTO public.quiztest_question(
+	id, "typeOfQ", title, difficulty, quiz_id)
+	VALUES (60,0,'What is Usain Bolt’s middle name?', 3, 1);
---
+++ sq_statements/update_ids.sql
@@ -0,0 +1,15 @@
+UPDATE public.quiztest_quizzes
+	SET id=2 
+	WHERE id=3;
+
+UPDATE public.quiztest_quizzes
+	SET id=3
+	WHERE id=4;
+
+UPDATE public.quiztest_quizzes
+	SET id=4
+	WHERE id=5;
+
+UPDATE public.quiztest_quizzes
+	SET id=5
+	WHERE id=6
