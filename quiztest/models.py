128d4d4253bb58c29669d82dea2b9a4e831df718
Ivy Ch <ivy.chepkwony@gmail.com>
2024-05-18 23:01:58 +0200

    putting them back

--- quiztest/models.py
+++ quiztest/models.py
@@ -85,9 +85,9 @@ class Answer(Updated):
     question = models.ForeignKey(
         Question, related_name='answer', on_delete=models.DO_NOTHING)
    
-    #answer_text = models.CharField( max_length=255, verbose_name=_("Answer Text"))
+    answer_text = models.CharField( max_length=255, verbose_name=_("Answer Text"))
     
-    #is_right = models.BooleanField(default=False)
+    is_right = models.BooleanField(default=False)
 
     def __str__(self):
         return self.answer_text #this needs to return a string variable
