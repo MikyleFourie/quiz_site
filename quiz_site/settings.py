b9f4df579f426f7db93b95359aeff74bdca97414
Jacques Coetzee <jcoetzee2201@gmail.com>
2024-06-08 16:37:41 +0200

    fixes?

--- quiz_site/settings.py
+++ quiz_site/settings.py
@@ -31,10 +31,6 @@ print(f"DEBUG is set to {DEBUG}")
 
 IS_HEROKU_APP = "DYNO" in os.environ and not "CI" in os.environ
 
-# SECURITY WARNING: don't run with debug turned on in production!
-if not IS_HEROKU_APP:
-    DEBUG = True
-
 # On Heroku, it's safe to use a wildcard for `ALLOWED_HOSTS``, since the Heroku router performs
 # validation of the Host header in the incoming HTTP request. On other platforms you may need to
 # list the expected hostnames explicitly in production to prevent HTTP Host header attacks. See:
