cf81d7360e5c8917feec3995b438b4b06c6a0262
Ivy Ch <ivy.chepkwony@gmail.com>
2024-06-09 14:53:26 +0200

    added science answers

---
+++ sq_statements/science_answers.sql
@@ -0,0 +1,131 @@
+INSERT INTO public.quiztest_answer(id, answer_text, question_id, is_right)
+VALUES
+    (304,'Deoxyribonucleic Acid',92,true),
+   	(305,'Deep Neuron Antibody',92,false),
+	(306,'Ribonucleic Acid',92,false),
+	(307,'Deoxyretanoic Acid',92,false),
+    
+	(308,'Oncologist',93,false),
+   	(309,'Ophthalmologist',93,true),
+	(310,'Paediatrician',93,false),
+	(311,'Retinician',93,false),
+   		
+	(312,'Rhinoceros Beetle',94,false),
+   	(313,'Leopard Tortoise',94,false),
+	(314,'Ant Lion',94,false),
+	(315,'Elephant Bird',94,true),
+
+	(316,'True',95,true),
+   	(317,'False',95,false),
+	
+	(318,'Hydrogen',96,false),
+	(319,'Oxygen',96,false),
+	(320,'Nitrogen ',96,true),
+	(321,'Helium',96,false),
+
+	(322,'Mol',97,false),
+	(323,'Pascal',97,false),
+	(324,'Amps',97,false),
+	(325,'Henry',97,true),
+	
+	(326,'True',98,false),
+	(327,'False',98,true),
+
+	(328,'Ear',99,true),
+	(329,'Nose',99,false),
+	(330,'Hands',99,false),
+	(331,'Feet',99,false),
+
+	(332,'True',100,true),
+	(333,'False',100,false),
+	
+	(334,'True',101,false),
+	(335,'False',101,true),
+	
+	(336,'Kidney',102,false),
+	(337,'Lungs',102,false),
+	(338,'Skin',102,true),
+	(339,'Liver',102,false),
+	
+	(340,'Pluto',103,false),
+	(341,'Neptune',103,true),
+	(342,'Mars',103,false),
+	(343,'Venus',103,false),
+	
+	(344,'Acetic',104,true),
+	(345,'Nitric',104,false),
+	(346,'Hyaluronic',104,false),
+	(347,'Citric',104,false),
+	
+	(348,'True',105,true),
+	(349,'False',105,false),
+	
+	(350,'Rats',106,false),
+	(351,'Fish',106,false),
+	(352,'Frogs',106,true),
+	(353,'Monkeys',106,false),
+	
+	(354,'True',107,false),
+	(355,'False',107,true),
+	
+	(356,'Vitamin A',108,false),
+	(357,'Vitamin D',108,true),
+	(358,'Vitamin K',108,false),
+	(359,'Vitamin E',108,false),
+	
+	(360,'KOH',109,false),
+	(361,'NaOH',109,false),
+	(362,'HCL',109,false),
+	(363,'NH3',109,true),
+	
+	(364,'Tomatoes',110,false),
+	(365,'Bread',110,false),
+	(366,'Onions',110,false),
+	(367,'Chickpeas',110,true),
+	
+	(368,'Hyper Text Markup Language',111,true),
+	(369,'Hyper Text Marking Language',111,false),
+	(370,'Hyper Trainer Markup Language',111,false),
+	(371,'Hyper Text Markup Leveler',111,false),
+
+	(372,'True',112,true),
+	(373,'False',112,false),
+	
+	(374,'200',113,false),
+	(375,'206',113,true),
+	(376,'246',113,false),
+	(377,'300',113,false),
+	
+	(378,'True',114,false),
+	(379,'False',114,true),
+	
+	(380,'Photosynthesis',115,false),
+	(381,'Mitochondria',115,false),
+	(382,'Chlorophyll ',115,true),
+	(383,'Ribosome',115,false),
+	
+	(384,'True',116,true),
+	(385,'False ',116,false),
+	
+	(386,'Derivation',117,false),
+	(387,'Multiplication',117,false),
+	(388,'Summation',117,false),
+	(389,'Integration',117,true),
+
+	(390,'True',118,false),
+	(391,'False',118,true),
+
+	(392,'28',119,false),
+	(393,'29',119,false),
+	(394,'30',119,true),
+	(395,'31',119,false),
+
+	(396,'8',120,false),
+	(397,'6',120,true),
+	(398,'4',120,false),
+	(399,'2',120,false),
+
+	(400,'Rabies',121,true),
+	(401,'Influenza',121,false),
+	(402,'Chickenpox',121,false),
+	(403,'Ebola',121,false);
