18c58a64fe17c2865ef53768c8acc6c79a40fdc2
Ivy Ch <ivy.chepkwony@gmail.com>
2024-06-10 16:55:55 +0200

    sql_sheets

---
+++ sq_statements/geography_answers.sql
@@ -0,0 +1,131 @@
+INSERT INTO public.quiztest_answer(id, answer_text, question_id, is_right)
+VALUES
+    (404,'Melbourne',122,false),
+   	(405,'Canberra',122,true),
+	(406,'Sydney',122,false),
+	(407,'Adelaide',122,false),
+    
+	(408,'Sri Lanka',123,false),
+   	(409,'India',123,true),
+	(410,'Spain ',123,false),
+	(411,'Portugal',123,false),
+   		
+	(412,'China',124,false),
+   	(413,'Peru',124,false),
+	(414,'Brazil',124,false),
+	(415,'Namibia',124,true),
+
+	(416,'True ',125,false),
+   	(417,'False',125,true),
+	
+	(418,'Tanzania',126,true),
+	(419,'South Africa',126,false),
+	(420,'Zimbabwe',126,false),
+	(421,'Egypt',126,false),
+
+	(422,'Dead Sea',127,false),
+	(423,'Crater Lake',127,false),
+	(424,'Don Juan Pond',127,true),
+	(425,'Walden Pond',127,false),
+	
+	(426,'Croatia',128,false),
+	(427,'Jamaica',128,false),
+	(428,'Mauritius',128,false),
+	(429,'Bahamas',128,true),
+	
+	(430,'True',129,true),
+	(431,'False',129,false),
+
+	(432,'True',130,true),
+	(433,'False',130,false),
+	
+	(434,'Ankara',131,true),
+	(435,'Istanbul',131,false),
+	(436,'Adana',131,false),
+	(437,'Antalya',131,false),
+
+	(438,'2',132,false),
+	(439,'3',132,false),
+	(440,'4',132,true),
+	(441,'5',132,false),
+
+	(442,'50',133,true),
+	(443,'51',133,false),
+	(444,'52',133,false),
+	(445,'54',133,false),
+	
+	(446,'True',134,true),
+	(447,'False',134,false),
+	
+	(448,'True ',135,false),
+	(449,'False',135,true),
+	
+	(450,'Amazon',136,false),
+	(451,'Mississippi',136,false),
+	(452,'Nile',136,true),
+	(453,'Thames',136,false),
+	
+	(454,'Hawaii',137,false),
+	(455,'Rhode Island',137,true),
+	(456,'Virginia',137,false),
+	(457,'Delaware',137,false),
+	
+	(458,'Australia',138,false),
+	(459,'Greenland',138,false),
+	(460,'Mauritius',138,false),
+	(461,'Madagascar',138,true),
+
+	(462,'Gauteng',139,true),
+	(463,'Western Cape',139,false),
+	(464,'KwaZulu Natal',139,false),
+	(465,'Free State',139,false),
+	
+	(466,'True',140,false),
+	(467,'False',140,true),
+	
+	(468,'Namibia',141,false),
+	(469,'United Kingdom ',141,false),
+	(470,'South Africa',141,true),
+	(471,'United States of America',141,false),
+
+	(472,'Le Serena',142,false),
+	(473,'Santiago',142,true),
+	(474,'Lima ',142,false),
+	(475,'Castro',142,false),
+	
+	(476,'Asia',143,false),
+	(477,'Antarctica',143,false),
+	(478,'Australia',143,false),
+	(479,'Africa',143,true),
+	
+	(480,'Dominica',144,true),
+	(481,'Cuba',144,false),
+	(482,'Democratic Republic of Congo',144,false),
+	(483,'Bhutan',144,false),
+	
+	(484,'True',145,true),
+	(485,'False',145,false),
+	
+	(486,'True',146,false),
+	(487,'False',146,true),
+	
+	(488,'191 ',147,false),
+	(489,'192 ',147,false),
+	(490,'193',147,true),
+	(491,'194',147,false),
+
+	(492,'Atlantic',148,false),
+	(493,'Pacific',148,true),
+	(494,'Indian ',148,false),
+	(495,'Arctic',148,false),
+
+	(496,'True',149,true),
+	(497,'False',149,false),
+	
+	(498,'Leamington Spa',150,false),
+	(499,'London',150,false),
+	(500,'Colchester',150,false),
+	(501,'Stratford Upon Avon',150,true),
+	
+	(502,'True ',151,false),
+	(503,'False',151,true);
---
+++ sq_statements/geography_questions.sql
@@ -0,0 +1,61 @@
+INSERT INTO quiztest_question(id, "typeOfQ", title, difficulty, quiz_id)
+VALUES
+    (122,0,'What is the capital city of Australia?',2,5),
+	
+   	(123,0,'The peacock is the national bird of which country?',3,5),
+	
+	(124,0,'Which of these countries does not have one of the 7 wonders of the world? ',2,5),
+	
+	(125,1,'South Africa has 4 capital cities.',1,5),
+    
+	(126,0,'In which country is Kilimanjaro found? ',2,5),
+	
+   	(127,0,'Which body of water has the highest salt water concentration? ',3,5),
+	
+	(128,0,'The flamingo is the national bird of which country?',2,5),
+	
+	(129,1,'South Africa has nine provinces. ',1,5),
+   		
+	(130,1,'Austria borders Italy. ',3,5),
+	
+   	(131,0,'What is the capital of Turkey?',3,5),
+	
+	(132,0,'How many colours does the Brazilian flag have? ',1,5),
+	
+	(133,0,'How many US states are there?',2,5),
+
+	(134,1,'The tallest mountain known to man is on Mars.',3,5),
+	
+   	(135,1,'The Taj Mahal is not one of the seven wonders of the world. ',1,5),
+	
+	(136,0,'What is the longest river in the world? ',1,5),
+	
+	(137,0,'What is the smallest US state by land area? ',2,5),
+	
+	(138,0,'What is the 4th largest island by land area?',3,5),
+	
+	(139,0,'Which South African province has the largest population? ',1,5),
+
+	(140,1,'China has the highest national population in the world. ',1,5),
+	
+	(141,0,'What country was Elon Musk born in?',1,5),
+	
+	(142,0,'What is the capital city of Chile?',3,5),
+	
+	(143,0,'Which continent is the only one to span over all four continents? ',1,5),
+	
+	(144,0,'Which country’s flag has the most purple in it?',2,5),
+	
+	(145,1,'The N2 highway stretches between Cape Town and Ermelo. ',3,5),
+
+	(146,1,'Isometric movement is a type of Earth movement.',2,5),
+	
+	(147,0,'How many countries are member states of the United Nations?',3,5),
+	
+	(148,0,'In which ocean is the Bermuda Triangle?',1,5),
+	
+	(149,1,'Belize’s flag has the most colours compared to all other national flags, using 12 colours in total. ',2,5),
+
+	(150,0,'Where was Shakespeare born? ',3,5),
+	
+	(151,1,'Croatia and Serbia are the only two countries that surround Bosnia.',3,5);
